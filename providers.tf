9baf54bd33628ce6092fc8cb3e2a3f3b7a918ed5
phesouza90 <104534810+phesouza90@users.noreply.github.com>
2022-05-28 11:24:57 -0300

    FIX

--- providers.tf
+++ providers.tf
@@ -10,6 +10,8 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
+  credentials = file("/home/vagrant/gcpkey.json")
+
   project = "lab-devops-cloud-351612"
   region  = "us-west1"
   zone    = "us-west1-b"
